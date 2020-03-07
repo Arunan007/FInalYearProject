c663f59a3e45ce004dc2bed094de03552084cc59
Bala Vignesh S <bala-pt3105@bala-pt3105.csez.zohocorpin.com>
2020-03-07 12:04:02 +0530

    Product form added

--- examples/index.js
+++ examples/index.js
@@ -32,6 +32,17 @@ function getMoveProductResponse(){
         alert("Data: " + data + "\nStatus: " + status);
       });
 }
+function loadDataforDashboard(){
+    $.get(portalUrl+":"+portalPortNumber+"/api/Participants", function(data, status){
+        // alert("Data: " + data + "\nStatus: " + status);
+        jQuery('#partcipantsCount').html(data.length);
+      });
+      $.get(portalUrl+":"+portalPortNumber+"/api/Product", function(data, status){
+        // alert("Data: " + data + "\nStatus: " + status);
+        jQuery('#productCountinDashboard').html(data.length);
+      });
+    getAllTransactions();
+}
 function getAllTransactions(){
      $.get(portalUrl+":"+portalPortNumber+"/api/system/historian", function(data, status){
         for(var i=0;i<5;i++){
@@ -52,7 +63,7 @@ function getAllTransactions(){
                         </tr>'
             jQuery('#transactionsTableBody').append($row);
         }      
-
+        jQuery("#transactionCountinDashboard").html(data.length);
     });
 }
 function getAllTransactionsPAGE(){
@@ -79,19 +90,57 @@ function getAllTransactionsPAGE(){
         }      
     });
 }
-function tableToggleDarkOrLightMode(){
+function tableToggleDarkOrLightMode(id){
     if(jQuery('#tableThemeMode')[0].checked==false){
-        jQuery('#tableDivCard').addClass('bg-default shadow');
+        jQuery('#'+id).addClass('bg-default shadow');
         jQuery('#tableThemeCardHeader').addClass('bg-transparent');
         jQuery('#tableThemeCardHeader h3').addClass('text-white');
         jQuery('#thead-id').removeClass('thead-light').addClass('thead-dark');
         jQuery('#tableId').addClass('table-dark');
     }
     if(jQuery('#tableThemeMode')[0].checked==true){
-        jQuery('#tableDivCard').removeClass('bg-default shadow');
+        jQuery('#'+id).removeClass('bg-default shadow');
         jQuery('#tableThemeCardHeader').removeClass('bg-transparent');
         jQuery('#tableThemeCardHeader h3').removeClass('text-white');
         jQuery('#thead-id').addClass('thead-light').removeClass('thead-dark');
         jQuery('#tableId').removeClass('table-dark');
+    }
+}
+function showAddProductForm(){
+    jQuery('#productsTableDivCard').hide();
+    jQuery('#addProductForm').show();
+    getLocation();
+}
+function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+      alert("Geolocation is not supported by this browser. Try adding Manually!");
+    }
+}
+function showPosition(position) {
+    jQuery('#addProductLatitudeField').val(position.coords.latitude);
+    jQuery('#addProductLongitudeField').val(position.coords.longitude);
+  }
+function addProductFormData(){
+    var addProductDataJson = {}
+    var proId=jQuery('#addProductIdField').val();
+    var proName=jQuery('#addProductNameField').val();
+    var proOwner=jQuery('#addProductOwnerField').val();
+    var proDesc=jQuery('#addProductDescriptionField').val();
+    var proAmount=jQuery('#addProductAmountField').val();
+    var proLati=jQuery('#addProductLatitudeField').val();
+    var proLongi=jQuery('#addProductLongitudeField').val();
+    if(proId != "" && proDesc!= "" && proName!="" &&  proOwner!="" && proAmount!="" && proLati!="" && proLongi!=""){
+        addProductDataJson["productId"]=proId;
+        addProductDataJson["productName"]=proName;
+        addProductDataJson["productOwner"]=proOwner;
+        addProductDataJson["productAmount"]=proAmount;
+        addProductDataJson["productLatitude"]=proLati;
+        addProductDataJson["productLongitude"]=proLongi;
+        addProductDataJson["productDesc"]=proDesc;
+    }
+    else{
+        alert("Please provide all the input");
     }
 }
